6342f4a72939504954c6df7e05628030a1a943ed
Brian Starkey <stark3y@gmail.com>
2023-01-13 22:21:56 +0000

    Fix help messages for subcommands

--- main.py
+++ main.py
@@ -38,31 +38,35 @@ def main():
     subparsers = parser.add_subparsers(title="Sub-commands")
 
     # Common arguments for tag handling commands
-    tag_cmd_parser = argparse.ArgumentParser(description="parent parser for tag commands")
+    tag_cmd_parser = argparse.ArgumentParser(description="parent parser for tag commands", add_help = False)
     tag_cmd_parser.add_argument('tag', help='Tag ID')
     tag_cmd_parser.add_argument('name', help='Tag owner name')
     tag_cmd_parser.add_argument('contact', help='Tag owner contact')
 
-    enrol_parser = subparsers.add_parser('enrol', add_help=False,
+    enrol_parser = subparsers.add_parser('enrol', add_help=True,
                                          parents=[tag_cmd_parser],
+                                         description='Add a tag in the database',
                                          help='Add a tag to the database')
     enrol_parser.set_defaults(func=enrol)
 
-    update_parser = subparsers.add_parser('update', add_help=False,
+    update_parser = subparsers.add_parser('update', add_help=True,
                                           parents=[tag_cmd_parser],
+                                          description='Update a tag in the database',
                                           help='Update a tag in the database')
     update_parser.set_defaults(func=update)
 
-    lookup_parser = subparsers.add_parser('lookup', add_help=False,
+    lookup_parser = subparsers.add_parser('lookup', add_help=True,
+                                          description='Look up a tag in the database',
                                           help='Look up a tag in the database')
     lookup_parser.add_argument('tag', help='Tag ID')
     lookup_parser.set_defaults(func=lookup)
 
-    label_parser = subparsers.add_parser('label', add_help=False,
+    label_parser = subparsers.add_parser('label', add_help=True,
+                                          description='Generate a label image',
                                           help='Generate a label image')
     label_parser.add_argument('--dpi', help='Image DPI', type=int, default=300)
-    label_parser.add_argument('-w', '--width_mm', help='Label width (mm)', type=int, default=89)
-    label_parser.add_argument('-h', '--height_mm', help='Label height (mm)', type=int, default=36)
+    label_parser.add_argument('--width_mm', help='Label width (mm)', type=int, default=89)
+    label_parser.add_argument('--height_mm', help='Label height (mm)', type=int, default=36)
     label_parser.add_argument('--out', help='Output filename (default: preview)', default=None)
     label_parser.add_argument('lines', help='Text lines to put on label', nargs='*')
     label_parser.set_defaults(func=label)
